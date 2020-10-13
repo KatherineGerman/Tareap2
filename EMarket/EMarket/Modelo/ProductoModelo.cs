6d022c2968e69f67894e967c3d5ff3379f31aa61
KatherineGerman <katherin03gt@gmail.com>
2020-10-13 14:20:00 -0400

    Declaracion de las variables y constructores (ProductoModelo)

--- EMarket/EMarket/Modelo/ProductoModelo.cs
+++ EMarket/EMarket/Modelo/ProductoModelo.cs
@@ -8,5 +8,25 @@ namespace EMarket.Modelo
 {
     class ProductoModelo
     {
+        public ProductoModelo(int idProducto, string nombreP, string codigoP, string stockP, DateTime fVencimiento, string descripcionP, string categoriaP, string estado)
+        {
+            IdProducto = idProducto;
+            NombreP = nombreP;
+            CodigoP = codigoP;
+            StockP = stockP;
+            FVencimiento = fVencimiento;
+            DescripcionP = descripcionP;
+            CategoriaP = categoriaP;
+            Estado = estado;
+        }
+
+        public int IdProducto { get; set; }
+        public string NombreP { get; set; }
+        public string CodigoP { get; set; }
+        public string StockP { get; set; }
+        public DateTime FVencimiento { get; set; }
+        public string DescripcionP { get; set; }
+        public string CategoriaP { get; set; }
+        public string Estado { get; set; }
     }
 }
