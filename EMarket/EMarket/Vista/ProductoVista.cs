7753d55f34fb870b0da5cde90f0ebdd0c43e6280
KatherineGerman <katherin03gt@gmail.com>
2020-10-13 14:18:26 -0400

    Enmaquetacion del proyecto

---
+++ EMarket/EMarket/Vista/ProductoVista.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EMarket.Vista
+{
+    public partial class ProductoVista : Form
+    {
+        public ProductoVista()
+        {
+            InitializeComponent();
+        }
+    }
+}
